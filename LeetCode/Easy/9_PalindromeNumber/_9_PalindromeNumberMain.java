5d22f02f6cbd4c8a783507aa1f8b0cb45d4cb6c8
Trung Dang <tdang229@uw.edu>
2017-03-30 22:26:48 -0700

    finished problems 13, 14, and 20

--- LeetCode/Easy/9_PalindromeNumber/_9_PalindromeNumberMain.java
+++ LeetCode/Easy/9_PalindromeNumber/_9_PalindromeNumberMain.java
@@ -5,14 +5,20 @@ public class _9_PalindromeNumberMain {
     public static boolean isPalindrome(int x) {
         if (x < 0) return false;
         else {
-
+            String s = x +"";
+            int i = 0, j = s.length() - 1;
+            while (i < s.length() && j >= 0) {
+                if (s.charAt(i) != s.charAt(j)) {
+                    return false;
+                }
+                i++; j--;
+            }
         }
-        return false;
-
+        return true;
     }
 
     public static void main(String...args) {
-        System.out.println(isPalindrome(123));
+        System.out.println(isPalindrome(1231));
     }
 
 }
