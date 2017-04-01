b7adf5d809430d785a170f31fbdb260a1f6ac7ff
Trung Dang <tdang229@uw.edu>
2017-04-01 12:36:55 -0700

    completed problem 21 merge two linked lists. Solution is not as optimal as recursive one on LeetCode

---
+++ LeetCode/Easy/_21_MergeTwoSortedLists/ListNode.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Trung on 4/1/2017.
+ */
+/**
+ * Definition for singly-linked list.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+
+}
